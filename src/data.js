f9fafe76fd845ccf7311e6ed3716ac075e1271b2
Jonathan Enudeme <47423105+Joefamouze@users.noreply.github.com>
2020-11-17 04:55:07 +0100

    share code on github

---
+++ src/data.js
@@ -0,0 +1,74 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'shirts',
+            image: './images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '2',
+            name: 'Balenciaga Shirt',
+            category: 'shirts',
+            image: './images/p2.jpg',
+            price: 150,
+            brand: 'Balenciaga',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '3',
+            name: 'Lacoste Shirt',
+            category: 'shirts',
+            image: './images/p3.jpg',
+            price: 150,
+            brand: 'Lacoste',
+            rating: 4.8,
+            numReviews: 18,
+            description: 'high quality product'
+        },
+        {
+            _id: '4',
+            name: 'Nike Slim Pants',
+            category: 'Pants',
+            image: './images/p4.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+
+        },
+        {
+            _id: '5',
+            name: 'Dior Pants',
+            category: 'Pants',
+            image: './images/p5.jpg',
+            price: 120,
+            brand: 'Dior',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '6',
+            name: 'TM Slim Pants',
+            category: 'Pants',
+            image: './images/p6.jpg',
+            price: 120,
+            brand: 'TM Lewin',
+            rating: 4.6,
+            numReviews: 13,
+            description: 'high quality product'
+        },
+
+    ],
+}
+
+export { data }
